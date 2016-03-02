a73d07135d46b10af9c5b42a8d9ea974b08f1cc5
Johan MARTIN <E145628R@U-INF-J-F222-05.iut-nantes.univ-nantes.prive>
2016-03-02 11:48:32 +0100

    ça marche

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ import sys
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
-def sportEqResearch(eq):
-    req = "select * from equipement where EquNom = '" + eq + "';"
+def sportEqResearch(eq, numLib):
+    if numLib == 0:
+        req = "select ComLib, EquNom from equipement where EquNom like '%" + eq + "%';"
+    else:
+        req = "select ComLib, EquNom from equipement where EquNom like '%" + eq + "%' and ComInsee like '" + str(numLib) + "%';"
     c.execute(req)
-    print (c.fetchone())
+    row = c.fetchone()
+    while row is not None:
+        print(row)
+        row = c.fetchone()
 
 os.system('clear')
 choice = ""
 while choice != exit:
-    eq = input("What's the name of the equipement sportif that you want to find ? \n")
-    sportEqResearch(eq)
-    if eq == "exit":
+    choice = input("What do you want to do ? \n - req to find an equipement sportif \n\n")
+    if choice == "req":
+        eq = input("What's the name of the equipement sportif that you want to find ? \n")
+        numLib = input("Departement : (0 : all departements)\n")
+        sportEqResearch(eq, int(numLib))
+    if choice == "exit":
         os.system('clear')
         sys.exit(0)
