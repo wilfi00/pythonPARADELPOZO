0fe29e69c311cdb06cdeff8cd9cf009df6663c19
Johan MARTIN <E145628R@U-INF-J-F222-05.iut-nantes.univ-nantes.prive>
2016-03-02 11:54:25 +0100

    sql request

--- main.py
+++ main.py
@@ -16,14 +16,24 @@ def sportEqResearch(eq, numLib):
         print(row)
         row = c.fetchone()
 
+def sqlRequest(req):
+    c.execute(req)
+    row = c.fetchone()
+    while row is not None:
+        print(row)
+        row = c.fetchone()
+
 os.system('clear')
 choice = ""
 while choice != exit:
-    choice = input("What do you want to do ? \n - req to find an equipement sportif \n\n")
+    choice = input("What do you want to do ? \n - req to find an equipement sportif \n - devMode to dev mode \n\n")
     if choice == "req":
         eq = input("What's the name of the equipement sportif that you want to find ? \n")
         numLib = input("Departement : (0 : all departements)\n")
         sportEqResearch(eq, int(numLib))
+    elif choice == "devMode":
+        req = input("Type your SQL select request : \n")
+        sqlRequest(req)
     if choice == "exit":
         os.system('clear')
         sys.exit(0)
