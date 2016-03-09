addbe8e66c8acb6e041844266437808ee396c33e
Johan MARTIN <E145628R@U-INF-J-F222-11.iut-nantes.univ-nantes.prive>
2016-03-09 10:31:28 +0100

    Nul

--- code/Recherche.py
+++ code/Recherche.py
@@ -1,8 +1,28 @@
+# -*- coding: utf-8 -*-
+import Activite
+import sqlite3
+"""import Mabite"""
+
 class Recherche :
-""" Classe qui effectue des recherches en utilisant les objets Activités et Equipements """
+    """ Classe qui effectue des recherches en utilisant les objets Activites et Equipements """
 
-    def activiteRecherche():
+    def __init__():
+        conn = sqlite3.connect('data.db')
+        c = conn.cursor()
 
+    def activiteRecherche(nomAct):
+        """
+        Methode qui recherche une activité avec son nom
+        Retourne son nom, le nom de la commune, l'ID de l'activité et l'ID de l'équipement associé
+        """
+        req = "select ActLib, ComLib, Id, EquipementId from activite where ActLib = '" + nomAct + "';"
+        c.execute(req)
+        row = c.fetchone()
+        while row is not None:
+            print(row)
+            row = c.fetchone()
+
+    """
     def sportEqResearch(eq, numLib):
         if numLib == 0:
             req = "select ComLib, EquNom from equipement where EquNom like '%" + eq + "%';"
@@ -13,3 +33,4 @@ class Recherche :
         while row is not None:
             print(row)
             row = c.fetchone()
+    """
