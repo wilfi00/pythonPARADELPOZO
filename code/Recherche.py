d1e8eab21430da0d85f683baf9a70dc6a64e93f8
Johan MARTIN <E145628R@U-INF-J-F222-12.iut-nantes.univ-nantes.prive>
2016-03-16 10:48:41 +0100

    adding exception :panda_face:

--- code/Recherche.py
+++ code/Recherche.py
@@ -18,15 +18,23 @@ class Recherche :
         Methode qui recherche une activité avec son nom
         Retourne son nom, le nom de la commune, l'ID de l'activité et l'ID de l'équipement associé
         """
-        req = "select ActLib, ComLib, Id, EquipementId from activite where ActLib like '%" + nomAct + "%';"
-        self.c.execute(req)
-        row = self.c.fetchone()
         listeAct = []
-        while row is not None:
-            ActLib, ComLib, Id, EquipementId = row
-            act = Activite(ActLib, ComLib, Id, EquipementId)
-            listeAct.append(act)
-            row = self.c.fetchone()
+        try:
+            req = "select ActLib, ComLib, Id, EquipementId from activite where ActLib like '%" + nomAct + "%';"
+        except TypeError:
+            print("Wrong input")
+        else:
+            try:
+                self.c.execute(req)
+            except sqlite3.Error:
+                print("SQL error")
+            else:
+                row = self.c.fetchone()
+                while row is not None:
+                    ActLib, ComLib, Id, EquipementId = row
+                    act = Activite(ActLib, ComLib, Id, EquipementId)
+                    listeAct.append(act)
+                    row = self.c.fetchone()
         return listeAct
 
 
@@ -35,43 +43,71 @@ class Recherche :
         Methode qui recherche une activité avec son nom
         Retourne son nom, le nom de la commune, l'ID de l'activité et l'ID de l'équipement associé
         """
-        req = "select ActLib, ComLib, Id, EquipementId from activite where ComLib like '%" + ComLib + "%';"
-        self.c.execute(req)
-        row = self.c.fetchone()
         listeAct = []
-        while row is not None:
-            ActLib, ComLib, Id, EquipementId = row
-            act = Activite(ActLib, ComLib, Id, EquipementId)
-            listeAct.append(act)
-            row = self.c.fetchone()
+        try:
+            req = "select ActLib, ComLib, Id, EquipementId from activite where ComLib like '%" + ComLib + "%';"
+        except TypeError:
+            print("Wrong input")
+        else:
+            try:
+                self.c.execute(req)
+            except sqlite3.Error:
+                print("SQL error")
+            else:
+                row = self.c.fetchone()
+                while row is not None:
+                    ActLib, ComLib, Id, EquipementId = row
+                    act = Activite(ActLib, ComLib, Id, EquipementId)
+                    listeAct.append(act)
+                    row = self.c.fetchone()
         return listeAct
 
     def sportEqResearchByEqu(self,equip):
         """
         Methode qui recherche un équipement par son nom
         """
-        req = "select ComInsee, ComLib, InsNom, EquipementId, EquNom from equipement where EquNom like '%" + equip + "%';"
-        self.c.execute(req)
-        row = self.c.fetchone()
         liste = []
-        while row is not None:
-            insee,comlib,insnom,equid,equnom = row
-            equ = Equipement(insee,comlib,insnom,equid,equnom)
-            liste.append(equ)
-            row = self.c.fetchone()
+        try:
+            req = "select ComInsee, ComLib, InsNom, EquipementId, EquNom from equipement where EquNom like '%" + equip + "%';"
+        except TypeError:
+            print("Wrong input")
+        else:
+            try:
+                self.c.execute(req)
+            except sqlite3.Error:
+                print("SQL error")
+            else:
+                row = self.c.fetchone()
+                while row is not None:
+                    insee,comlib,insnom,equid,equnom = row
+                    equ = Equipement(insee,comlib,insnom,equid,equnom)
+                    liste.append(equ)
+                    row = self.c.fetchone()
         return liste
 
     def sportEqResearchByDep(self,dep):
         """
         Methode qui recherche un équipement par son département
         """
-        req = "select ComInsee, ComLib, InsNom, EquipementId, EquNom from equipement where ComInsee like '%" + dep + "%';"
-        self.c.execute(req)
-        row = self.c.fetchone()
         liste = []
-        while row is not None:
-            insee,comlib,insnom,equid,equnom = row
-            equ = Equipement(insee,comlib,insnom,equid,equnom)
-            liste.append(equ)
-            row = self.c.fetchone()
+        try:
+            req = "select ComInsee, ComLib, InsNom, EquipementId, EquNom from equipement where ComInsee like '%" + dep + "%';"
+        except TypeError:
+            print("Wrong input")
+        else:
+            try:
+                self.c.execute(req)
+
+            except sqlite3.Error:
+                print("SQL error")
+            else:
+                row = self.c.fetchone()
+                while row is not None:
+                    insee,comlib,insnom,equid,equnom = row
+                    equ = Equipement(insee,comlib,insnom,equid,equnom)
+                    liste.append(equ)
+                    row = self.c.fetchone()
         return liste
+
+rechercheTest = Recherche()
+rechercheTest.sportEqResearchByDep("3")
